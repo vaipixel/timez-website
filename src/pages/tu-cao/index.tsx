f52a8cd78bf50400b19eb2f24805708260455145
渣渣辉 <1@fxcdev.com>
2021-12-05 17:29:15 +0800

    add tucao proxy

---
+++ src/pages/tu-cao/index.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import {TuCaoUtil} from "@site/src/utils";
+
+export default class TuCao extends Component {
+
+    componentDidMount() {
+        let params = new URLSearchParams(window.location.search)
+        let data: any = {
+                "nickname": params.get("nickname"),
+                "avatar": params.get("avatar"),
+                "openid": params.get("openid"),
+            },
+            productId = 1221;
+        TuCaoUtil.request(productId, data);
+    }
+
+    render() {
+        return <span>跳转中...</span>;
+    }
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export {default as TuCaoUtil} from './tucaoUtil';
